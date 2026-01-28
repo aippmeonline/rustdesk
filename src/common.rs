fbe6a4e70a100332902a97773dcd51fba54f5846
aippmeonline <aippme@hotmail.com>
2026-01-28 15:51:59 +0800

    更新配置文件

--- src/common.rs
+++ src/common.rs
@@ -1050,7 +1050,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://124.220.38.249".to_owned()
 }
 
 #[inline]
