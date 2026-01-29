218ccbbdbe7050be90d64943a617deeceb7a0f6b
aippmeonline <aippme@hotmail.com>
2026-01-29 08:31:44 +0800

    更新配置文件

--- src/common.rs
+++ src/common.rs
@@ -1050,7 +1050,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://rs.ailot.top".to_owned()
+    "http://rs.ailot.top:21114".to_owned()
 }
 
 #[inline]
